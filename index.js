ae81d7109cb5c9061397673612d677b32bb3abe3
jonschlinkert <jon.schlinkert@sellside.com>
2017-03-02 08:49:31 -0500

    pass `thisArg` to iterator

--- index.js
+++ index.js
@@ -13,13 +13,12 @@ var map = require('arr-map');
 
 module.exports = function(collection, fn, thisArg) {
   if (!Array.isArray(collection)) {
-    var iterator = makeIterator(fn);
+    var iterator = makeIterator(fn, thisArg);
     var result = [];
 
     forOwn(collection, function(value, key) {
       result.push(iterator(value, key, collection));
     });
-
     return result;
   }
   return map(collection, fn, thisArg);
